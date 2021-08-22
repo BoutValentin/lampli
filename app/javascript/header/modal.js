e73500e86301ce0b9dd82a48bba57a4e4a7c9acf
Valentin Bout <bout.valentin@gmail.com>
2021-08-22 10:36:40 +0200

    Feat: third commit

---
+++ app/javascript/header/modal.js
@@ -0,0 +1,22 @@
+// Handle header open
+const ready = () => {
+    console.log('wee')
+    const header_open_button = document.querySelector('.open-big-header .open-header-img');
+    const header_close_button = document.querySelector('.big-header .header-modal-top .close-big-header');
+    
+    const header_modal = document.querySelector('.big-header');
+    
+    const header_little_screen = document.querySelector('header .litle-screen');
+
+    const abstract_toggle = (str) => {
+        header_modal.classList.toggle('hidden');
+        header_little_screen.classList.toggle('hidden');
+    }
+    
+    const hide = () => abstract_toggle('add');
+    const show = () => abstract_toggle('remove');
+    
+    header_open_button.addEventListener("click", show);
+    header_close_button.addEventListener("click", hide);
+}
+document.addEventListener('turbolinks:load', ready)
